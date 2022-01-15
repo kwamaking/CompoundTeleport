dd34d07d0e6c3e2775a7a1e2c1f07c02e7c77173
kwamaking <kwamaking@gmail.com>
2022-01-14 21:56:57 -0600

    Refactored the original plugin, added the ability to include your own monuments.

--- CompoundTeleport.cs
+++ CompoundTeleport.cs
@@ -1,243 +1,230 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 
 namespace Oxide.Plugins
 {
     [Info("Compound Teleport", "kwamaking", "2.0.0")]
-    [Description("Teleport through the death screen to the NPC town and bandit camp")]
+    [Description("Teleport through the death screen to Bandit Camp, Outpost, or any configured monument.")]
     class CompoundTeleport : RustPlugin
     {
         #region Variables
-        private int Layer = ~(1 << 8 | 1 << 10 | 1 << 18 | 1 << 21 | 1 << 24 | 1 << 28 | 1 << 29);
-        private Dictionary<string, Vector3> positions = new Dictionary<string, Vector3>();
-        private Dictionary<BasePlayer, SleepingBag[]> bags = new Dictionary<BasePlayer, SleepingBag[]>();
-        private Queue<SleepingBag> bagsPool = new Queue<SleepingBag>();
-        List<Vector3> PositionsOutPost = new List<Vector3>();
-        List<Vector3> PositionsBandit = new List<Vector3>();
+
+        private const string UsePermission = "compoundteleport.use";
+        private const string OutPostPrefab = "assets/bundled/prefabs/autospawn/monument/medium/compound.prefab";
+        private const string BanditTownPrefab = "assets/bundled/prefabs/autospawn/monument/medium/bandit_town.prefab";
+        private const int TeleportLayer = ~(1 << 8 | 1 << 10 | 1 << 18 | 1 << 21 | 1 << 24 | 1 << 28 | 1 << 29);
+        private const int TeleportRadius = 50;
+        private const int Timer = 150;
+        private const int MaxAttemptsToFindTeleportLocation = 25;
+        private const float MaxDistance = 100f;
+        private const float MaxDistanceFromGround = 100f;
+        private readonly string[] ValidTeleportColliders = { "carpark", "concrete_slabs", "road", "train_track", "pavement", "platform", "walkway", "helipad" };
+        private Dictionary<ulong, List<SleepingBag>> playerTeleportLocations = new Dictionary<ulong, List<SleepingBag>>();
+        private CompoundTeleportConfiguration pluginConfiguration = new CompoundTeleportConfiguration();
 
         #endregion
 
-        #region Config
+        #region Oxide Hooks
 
-        private static Configuration config = new Configuration();
-        private class Configuration
+        private void Init()
         {
-            [JsonProperty("Sleeping bag Cooldown")]
-            public int cooldown;
-            [JsonProperty("The opportunity respawn in a outpost")]
-            public bool outPostRespawn;
-            [JsonProperty("Name bag outpost")]
-            public string bagNameOutPost;
-            [JsonProperty("The opportunity respawn in a Bandit Town")]
-            public bool banditRespawn;
-            [JsonProperty("Name bag Bandit Town")]
-            public string bagNameBandit;
-            public static Configuration GetNewConfiguration()
-            {
-                return new Configuration
-                {
-                    cooldown = 150,
-                    outPostRespawn = true,
-                    bagNameOutPost = "OUTPOST",
-                    banditRespawn = true,
-                    bagNameBandit = "BANDIT TOWN"
-
-                };
-            }
-        }
-
-        protected override void LoadConfig()
-        {
-            base.LoadConfig();
             try
             {
-                config = Config.ReadObject<Configuration>();
+                permission.RegisterPermission(UsePermission, this);
+                pluginConfiguration = Config.ReadObject<CompoundTeleportConfiguration>();
             }
-            catch
+            catch (Exception e)
             {
-                PrintWarning("Error reading configuration 'oxide/config/{Name}' Check, please");
-                Unsubscribe("OnPlayerRespawn");
-                Unsubscribe("OnServerCommand");
-                Unsubscribe("OnPlayerConnected");
-                Unsubscribe("OnPlayerDisconnected");
+                base.LoadConfig();
+                Puts($"Failed to load Compound Teleport configuration, using default configuration: {e.Message} {e.StackTrace}");
             }
-            NextTick(SaveConfig);
+
+            CreateTeleportLocationsForActivePlayers();
+
         }
+
         protected override void LoadDefaultConfig()
         {
-            config = new Configuration();
+            pluginConfiguration = CompoundTeleportConfiguration.BuildDefaultConfiguration();
             SaveConfig();
         }
-        protected override void SaveConfig() => Config.WriteObject(config);
 
+        protected override void SaveConfig() => Config.WriteObject(pluginConfiguration);
 
-        #endregion
+        private void OnPlayerConnected(BasePlayer player) => CreateTeleportLocations(player);
 
-        #region OxideHooks
+        private void OnPlayerDisconnected(BasePlayer player) => DestroyTeleportLocations(player);
+
+        private void OnPlayerRespawn(BasePlayer player, SleepingBag bag)
+        {
+            if (!playerTeleportLocations.ContainsKey(player.userID)) return;
+
+            if (playerTeleportLocations[player.userID].Any(b => b.net.ID == bag.net.ID))
+                bag.transform.position = AttemptToFindTeleportPosition(player, bag.transform.position);
+        }
 
         void Unload()
         {
-            foreach (SleepingBag[] bagsToRemove in bags.Values)
-                for (int i = 0; i < bagsToRemove.Length; i++)
-                    bagsToRemove[i]?.Kill();
-            foreach (SleepingBag bagsToRemove in bagsPool)
-                bagsToRemove?.Kill();
-        }
+            playerTeleportLocations.Keys.ToList().ForEach(
+                playerId => playerTeleportLocations[playerId].ForEach(bag => bag.Kill())
+            );
 
-        void OnPlayerRespawn(BasePlayer p, SleepingBag bag)
-        {
-            foreach (SleepingBag findBagPlayer in bags[p])
-            {
-                if (findBagPlayer.net.ID == bag.net.ID)
-                {
-                    var pos = bag.niceName == config.bagNameBandit ? PositionsBandit : PositionsOutPost;
-                    bag.transform.position = pos.GetRandom();
-                }
-            }
-        }
-
-        object OnServerCommand(ConsoleSystem.Arg arg)
-        {
-            BasePlayer basePlayer = arg.Player();
-            if (basePlayer == null)
-                return null;
-            uint netId = arg.GetUInt(0, 0);
-            if (arg.cmd.Name.ToLower() == "respawn_sleepingbag_remove" && netId != 0)
-            {
-                foreach (SleepingBag noRemoveBags in bags[basePlayer])
-                    if (noRemoveBags.net.ID == netId)
-                        return false;
-            }
-            return null;
-        }
-
-        private void OnServerInitialized()
-        {
-            if (ConVar.Server.level == "HapisIsland" && config.outPostRespawn)
-            {
-                SpawnPointGeneration(new Vector3(-211f, 105f, -432f), 40, PositionsOutPost, "carpark", "concrete_slabs", "road", "train_track", "pavement", "platform");
-                positions.Add(config.bagNameOutPost, new Vector3(-211f, 105f, -432f));
-            }
-            else
-            {
-                foreach (MonumentInfo monument in TerrainMeta.Path.Monuments)
-                {
-                    if (monument.name.ToLower() == "assets/bundled/prefabs/autospawn/monument/medium/compound.prefab" && config.outPostRespawn)
-                    {
-                        SpawnPointGeneration(monument.transform.position, 40, PositionsOutPost, "carpark", "concrete_slabs", "road", "train_track", "pavement", "platform");
-                        positions.Add(config.bagNameOutPost, monument.transform.position);
-                    }
-                    else if (monument.name.ToLower() == "assets/bundled/prefabs/autospawn/monument/medium/bandit_town.prefab" && config.banditRespawn)
-                    {
-                        SpawnPointGeneration(monument.transform.position, 75, PositionsBandit, "helipad", "walkway", "rope", "floating");
-                        positions.Add(config.bagNameBandit, monument.transform.position);
-                    }
-                }
-            }
-
-            foreach (BasePlayer player in BasePlayer.activePlayerList)
-                OnPlayerConnected(player);
-        }
-
-        private void OnPlayerConnected(BasePlayer d)
-        {
-            int count = positions.Count, idx = -1;
-
-            if (!bags.ContainsKey(d))
-                bags.Add(d, new SleepingBag[count]);
-
-            foreach (var positionKvp in positions)
-            {
-                SleepingBag bag = FromPool(d);
-                if (bag == null) continue;
-                bag.niceName = positionKvp.Key;
-                bag.transform.position = positionKvp.Value;
-
-                bags[d][++idx] = bag;
-
-                SleepingBag.sleepingBags.Add(bag);
-            }
-        }
-
-
-        private void OnPlayerDisconnected(BasePlayer d)
-        {
-            if (!bags.ContainsKey(d))
-                return;
-
-            foreach (SleepingBag bag in bags[d])
-            {
-                SleepingBag.sleepingBags.Remove(bag);
-                ResetToPool(bag);
-            }
-        }
-
-        #endregion
-
-        #region Metods generate spawn point
-
-        public void SpawnPointGeneration(Vector3 pos, float radius, List<Vector3> targetPosList, params string[] coliderName)
-        {
-            for (int i = 0; i < 150; i++)
-            {
-                RaycastHit rayHit;
-                Vector3 resultPositions = pos + (Random.insideUnitSphere * radius);
-                resultPositions.y = pos.y + 100f;
-                Puts("raycast hit?");
-                Puts(Physics.Raycast(resultPositions, Vector3.down, out rayHit, 100, Layer, QueryTriggerInteraction.Ignore).ToString());
-                Puts(rayHit.collider.name);
-                if (Physics.Raycast(resultPositions, Vector3.down, out rayHit, 100, Layer, QueryTriggerInteraction.Ignore))
-                {
-                    if (rayHit.collider is TerrainCollider)
-                        targetPosList.Add(rayHit.point);
-                    else
-                    {
-                        if (coliderName != null)
-                            for (int a = 0; a < coliderName.Length; a++)
-                            {
-                                Puts(rayHit.collider.name);
-                                if (rayHit.collider.name.Contains(coliderName[a]))
-                                    targetPosList.Add(rayHit.point);
-                            }
-                    }
-                }
-            }
+            playerTeleportLocations.Clear();
         }
 
         #endregion
 
         #region Helpers
-        private SleepingBag FromPool(BasePlayer d)
+
+        private void CreateTeleportLocationsForActivePlayers()
         {
-            SleepingBag bag;
-
-            if (bagsPool.Count > 0)
+            try
             {
-                bag = bagsPool.Dequeue();
-                bag.OwnerID = d.userID;
+                BasePlayer
+                .activePlayerList
+                .ToList()
+                .ForEach(player => CreateTeleportLocations(player));
+            }
+            catch (Exception e)
+            {
+                Puts($"Failed to create teleport locations for active players: {e.Message} {e.StackTrace} ");
+            }
+        }
 
-                return bag;
+        private void CreateTeleportLocations(BasePlayer player)
+        {
+            if (!permission.UserHasPermission(player.UserIDString, UsePermission)) return;
+
+            DestroyTeleportLocations(player);
+
+            playerTeleportLocations.Add(player.userID, new List<SleepingBag>());
+
+            pluginConfiguration.teleportLocations.ForEach(teleportLocation =>
+            {
+                var teleportPosition = FindMonumentPosition(teleportLocation.monumentPrefab);
+                if (null != teleportPosition && teleportLocation.enabled)
+                    playerTeleportLocations[player.userID].Add(
+                        CreateTeleportLocation(player, teleportLocation, (Vector3)teleportPosition)
+                    );
+            });
+        }
+
+        private void DestroyTeleportLocations(BasePlayer player)
+        {
+            if (!playerTeleportLocations.ContainsKey(player.userID))
+                return;
+
+            playerTeleportLocations[player.userID].ForEach(bag => bag.Kill());
+            playerTeleportLocations.Remove(player.userID);
+        }
+
+        private Vector3 FindMonumentPosition(string monumentPrefab)
+        {
+
+            var monumentPosition = TerrainMeta.Path.Monuments
+            .Where(m => m.name.ToLower() == monumentPrefab)
+            .FirstOrDefault()?
+            .transform
+            .position;
+
+            if (null == monumentPosition)
+                Puts($"Failed to find a location for Monument: {monumentPrefab}");
+
+            return monumentPosition ?? new Vector3();
+        }
+
+        private Vector3 AttemptToFindTeleportPosition(BasePlayer player, Vector3 monumentLocation)
+        {
+            for (int i = 0; i < MaxAttemptsToFindTeleportLocation; i++)
+            {
+                RaycastHit rayHit;
+                Vector3 positionAttempt = monumentLocation + (UnityEngine.Random.insideUnitSphere * TeleportRadius);
+                positionAttempt.y = monumentLocation.y + MaxDistanceFromGround;
+                if (Physics.Raycast(positionAttempt, Vector3.down, out rayHit, MaxDistance, TeleportLayer, QueryTriggerInteraction.Ignore))
+                {
+                    if (rayHit.collider is TerrainCollider)
+                        return rayHit.point;
+                    else if (ValidTeleportColliders.Any(c => rayHit.collider.name.Contains(c)))
+                        return rayHit.point;
+                }
             }
 
-            GameObject go = new GameObject();
-            bag = go.AddComponent<SleepingBag>();
+            Puts($"Failed to find a spawn location for {player.displayName}, using default spawn.");
 
-            bag.deployerUserID = d.userID;
-            bag.net = Network.Net.sv.CreateNetworkable();
-            bag.niceName = string.Empty;
-            bag.secondsBetweenReuses = config.cooldown;
-            bag.transform.position = Vector3.one;
-            bag.RespawnType = ProtoBuf.RespawnInformation.SpawnOptions.RespawnType.Bed;
-            bag.unlockTime = 0;
-
-            return bag;
+            return ServerMgr.FindSpawnPoint(player).pos;
         }
 
-        private void ResetToPool(SleepingBag bag)
+        private SleepingBag CreateTeleportLocation(BasePlayer player, TeleportLocation teleportLocation, Vector3 position)
         {
-            bagsPool.Enqueue(bag);
+            GameObject gameObject = new GameObject();
+            SleepingBag sleepingBag = gameObject.AddComponent<SleepingBag>();
+
+            sleepingBag.deployerUserID = player.userID;
+            sleepingBag.net = Network.Net.sv.CreateNetworkable();
+            sleepingBag.niceName = teleportLocation.name;
+            sleepingBag.secondsBetweenReuses = teleportLocation.timer;
+            sleepingBag.transform.position = position;
+            sleepingBag.RespawnType = ProtoBuf.RespawnInformation.SpawnOptions.RespawnType.Bed;
+            sleepingBag.unlockTime = 0;
+
+            SleepingBag.sleepingBags.Add(sleepingBag);
+
+            return sleepingBag;
         }
+
+        #endregion
+
+        #region Config Classes
+        private class CompoundTeleportConfiguration
+        {
+            [JsonProperty("teleportLocations")]
+            public List<TeleportLocation> teleportLocations { get; set; }
+
+            public static CompoundTeleportConfiguration BuildDefaultConfiguration()
+            {
+                return new CompoundTeleportConfiguration
+                {
+                    teleportLocations = TeleportLocation.BuildDefaultTeleportLocations()
+                };
+            }
+        }
+        private class TeleportLocation
+        {
+            [JsonProperty("name")]
+            public string name { get; set; }
+            [JsonProperty("timer")]
+            public int timer { get; set; }
+            [JsonProperty("monumentPrefab")]
+            public string monumentPrefab { get; set; }
+            [JsonProperty("enabled")]
+            public bool enabled { get; set; }
+
+            public static List<TeleportLocation> BuildDefaultTeleportLocations()
+            {
+
+                return new List<TeleportLocation>
+                {
+                    new TeleportLocation
+                    {
+                        name = "Outpost",
+                        timer = Timer,
+                        monumentPrefab = OutPostPrefab,
+                        enabled = true
+                    },
+                    new TeleportLocation
+                    {
+                        name = "Bandit Camp",
+                        timer = Timer,
+                        monumentPrefab = BanditTownPrefab,
+                        enabled = true
+                    }
+                };
+            }
+        }
+
         #endregion
     }
 }
